d9c233e9d721b064615143dbaf49a8807e06c42d
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-13 11:28:06 +0300

    Abstract oluştuldu

---
+++ PizzapanDataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PizzapanDataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetList();
+        T GetById(int id);
+    }
+}
