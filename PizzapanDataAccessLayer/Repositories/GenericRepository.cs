c7731317d8cef62e3948330ff1efe9c8a5f88a70
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-13 12:00:31 +0300

    GenericRepository dolduruldu

---
+++ PizzapanDataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using PizzapanDataAccessLayer.Abstract;
+using PizzapanDataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzapanDataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+       
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
