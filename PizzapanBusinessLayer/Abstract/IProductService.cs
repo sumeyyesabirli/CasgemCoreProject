96d91e22022fb40569fc521b0e399650ad8c176d
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-13 14:10:35 +0300

    Admin Layout

---
+++ PizzapanBusinessLayer/Abstract/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using PizzapanEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzapanBusinessLayer.Abstract
+{
+    public interface IProductService : IGenericService<Product>
+    {
+    }
+}
---
+++ PizzapanBusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,42 @@
+﻿using PizzapanBusinessLayer.Abstract;
+using PizzapanDataAccessLayer.Abstract;
+using PizzapanEntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace PizzapanBusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void TDelete(Category entity)
+        {
+            _categoryDal.Delete(entity);
+        }
+
+        public Category TGetById(int id)
+        {
+            return _categoryDal.GetById(id);
+        }
+
+        public List<Category> TGetList()
+        {
+            return _categoryDal.GetList();
+        }
+
+        public void TInsert(Category entity)
+        {
+            _categoryDal.Insert(entity);
+        }
+
+        public void TUpdate(Category entity)
+        {
+            _categoryDal.Update(entity);
+        }
+    }
+}
