0620a7c4fc354ebaa59888f9d55dfed7bca7045f
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-13 12:14:38 +0300

    BusinessLayer oluşturuldu

---
+++ PizzapanDataAccessLayer/EntityFramework/EfContactDal.cs
@@ -0,0 +1,15 @@
+﻿using PizzapanDataAccessLayer.Abstract;
+using PizzapanDataAccessLayer.Repositories;
+using PizzapanEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzapanDataAccessLayer.EntityFramework
+{
+    public class EfContactDal : GenericRepository<Contact>, IContactDal
+    {
+    }
+}
