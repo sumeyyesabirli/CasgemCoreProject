b0dbfd8325ac6f13b8256de2f006f2fcc17de19a
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-13 11:06:34 +0300

    Migration oluşturuldu

--- PizzapanDataAccessLayer/Concrete/Context.cs
+++ PizzapanDataAccessLayer/Concrete/Context.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PizzapanEntityLayer.Concrete;
 
 namespace PizzapanDataAccessLayer.Concrete
 {
-    internal class Context
+    public class Context:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SUM;initial catalog=CasgemDbPizzaPan;integrated security=true");
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
