57464a37900b9b3e68870c65ec4e1e9d3de1f7ed
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-21 11:33:38 +0300

    Best Pizza ve Description

---
+++ PizzapanPresentationLayer/ViewComponents/Default/_Description.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzapanPresentationLayer.ViewComponents.Default
+{
+    public class _Description : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+  
+}
